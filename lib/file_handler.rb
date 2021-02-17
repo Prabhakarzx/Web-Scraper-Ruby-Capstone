c28c66736583fef0367e11fd3eb21a2706fe04d8
prabhakarzx <ppandey2098@gmail.com>
2021-02-17 19:03:45 +0530

    setup the file handler

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -12,7 +12,27 @@ class FileHandler
     File.write("#{TEAM_DIR}#{@file_name}.json", JSON.pretty_generate(@hash))
   end
 
-  
+  def players_to_json
+    dir_maker(PLAYERS_DIR)
+    players_file_name = "#{@file_name}.json"
+
+    if File.file?("#{PLAYERS_DIR}#{players_file_name}") && File.read("#{PLAYERS_DIR}#{players_file_name}") != ''
+      file = JSON.parse(File.read("#{PLAYERS_DIR}#{players_file_name}"))
+      file.merge!(@hash)
+      File.write("#{PLAYERS_DIR}#{players_file_name}", JSON.pretty_generate(file))
+    else
+      File.write("#{PLAYERS_DIR}#{players_file_name}", JSON.pretty_generate(@hash))
+    end
+  end
+
+  def self.file_reader(dir_type, file_name)
+    return JSON.parse(File.read("#{TEAM_DIR}#{file_name}.json")) if dir_type == 'CLUBS'
+    return JSON.parse(File.read(file_name)) if dir_type == 'PLAYERS'
+  end
+
+  def self.access_all_players_files
+    Dir["#{PLAYERS_DIR}*.json"]
+  end
 
   private
 
