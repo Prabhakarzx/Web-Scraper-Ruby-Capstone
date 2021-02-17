dac7fb3584a39fab0d11f227b2a16b7c51070fd8
prabhakarzx <ppandey2098@gmail.com>
2021-02-17 20:32:15 +0530

    Add funtions to player and player rating scrap files

--- lib/player_scraper.rb
+++ lib/player_scraper.rb
@@ -13,4 +13,26 @@ class PlayerScraper < Parser
     @url_hash = {}
     add_clubs_url
   end
+
+  private
+
+  def add_clubs_url
+    team_json_file = FileHandler.file_reader(DIR_TYPE, @league)
+    team_json_file.each do |club, attrib|
+      @url_hash.merge!(club => SITE + attrib['url'])
+    end
+    parse_players
+  end
+
+  def parse_players
+    @url_hash.each_with_index do |(team_name, url), index|
+      @url = url
+      parsed_url = parse
+      parsed_url.xpath('//comment()').each { |comment| comment.replace(comment.text) }
+      print_player_node(team_name, index)
+      scrap_players(parsed_url, team_name)
+    end
+    PlayersRating.new
+  end
+
 end
--- lib/players_rating.rb
+++ lib/players_rating.rb
@@ -8,4 +8,20 @@ class PlayersRating
     @file_name = nil
     filter_file_names
   end
+
+  def filter_file_names
+    @players_file_array.length.times do |i|
+      @file_name = @players_file_array[i]
+      filter_players(FileHandler.file_reader(DIR_TYPE, @file_name))
+    end
+  end
+
+  def filter_players(player_hash)
+    player_hash.each do |_club, player|
+      player.each do |_player_name, attrib|
+        rate_players(attrib)
+      end
+    end
+    FileHandler.new(player_hash, File.basename(@file_name, '.json')).players_to_json
+  end
 end
