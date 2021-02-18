db4ea0b03e4bd1d09f46ea6a4974d8d03e03b357
prabhakarzx <ppandey2098@gmail.com>
2021-02-18 11:29:02 +0530

    work on plyer rating add filters and rate function

--- lib/players_rating.rb
+++ lib/players_rating.rb
@@ -24,4 +24,15 @@ class PlayersRating
     end
     FileHandler.new(player_hash, File.basename(@file_name, '.json')).players_to_json
   end
+
+  def rate_players(attrib)
+    attacker_rating = attrib['Goals'].to_f + attrib['Assists'].to_f
+    defender_rating = attrib['Tackles_won'].to_f + attrib['Int_&_blks'].to_f + attrib['Clearances'].to_f - (attrib['Errors'].to_f * 10)
+    if attrib['Position'] == 'GK'
+      gk_rating = (attrib['Save_%'].to_f * 100) + (attrib['Clean_sheet'].to_f * 10) - attrib['Goals_conceded'].to_f
+      attrib.merge!('Goal_keeper_rating' => gk_rating.round(3))
+    end
+    attrib.merge!('Attacker_rating' => attacker_rating.round(3))
+    attrib.merge!('Defender_rating' => defender_rating.round(3))
+  end
 end
